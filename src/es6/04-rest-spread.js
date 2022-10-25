ebfa6589110d903d9960c1f056249b1cc0709b3f
will_08 <wbahoque71@gmail.com>
2022-10-25 17:58:10 -0500

    Aprendi spread operartor con desestructuracion de objetos y arrays

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,21 @@ console.log(a, fruits[1]); //Imprimiendo  de forma natural
 let user = {username: 'Oscar', age: 34};
 let { username, age } = user;
 console.log(username, age);
+
+// spread operator 
+
+let person = {name: 'oscar', age: 28};
+let country = 'MX';
+
+let data = {id: 1, ...person, country}
+console.log(data);
+
+// rest
+// values se declara para recoger el resto de los valores
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1,1,2,3);

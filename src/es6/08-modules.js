428b47659e77bb04c2651d2f27a05e27e468f843
will_08 <wbahoque71@gmail.com>
2022-10-26 16:47:15 -0500

    Modificacion del package.json y se acaba de aprender modulos de exportacion y importacion en js

---
+++ src/es6/08-modules.js
@@ -0,0 +1,3 @@
+import hello from './modules.js'; // importacion del archivo module
+
+hello();
---
+++ src/es6/modules.js
@@ -0,0 +1,6 @@
+// Declaracion de la funcion flecha para exportar al archivo 08-modules.js
+const hello = () => {
+    console.log('Hello');
+}
+
+export default hello; // Una de las formas de exportar archivos

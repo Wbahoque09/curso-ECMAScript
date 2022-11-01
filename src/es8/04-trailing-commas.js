0fe98cb80c315f505472262604868e305b7d9b1c
will_08 <wbahoque71@gmail.com>
2022-11-01 16:21:28 -0500

    Aprendi sobre string padding y trailing commas

---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,19 @@
+const ages = [24, 21, 90, 87, , , ,41];
+
+console.log(ages);
+console.log(ages.length);
+
+// Trailing commas
+// Las trailing commas consisten en comas al final de objetos o arrays que faciliten añadir nuevos elementos y evitar errores de sintaxis.
+
+// const usuario = {
+//     name: 'Andres',
+//     email: "andres@correo.com",
+//     age: 23, //<-- Trailing comma
+// }
+
+// const nombres = [
+//     "Andres",
+//     "Valeria",
+//     "Jhesly", //<-- Trailing comma
+//  ]

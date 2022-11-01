ef249ba65af61b11a2ba30fa6c22a974907c3d06
will_08 <wbahoque71@gmail.com>
2022-11-01 15:40:07 -0500

    es8, SE aprendio sobre las clases object values-keys-entries

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,21 @@
+const countries = {MX: 'México', CD: 'Canada', CL: 'Columbia', BL: 'Bolivia'};
+
+console.log(Object.entries(countries));
+
+// Obtener los pares de valor de un objeto en un array
+// Object.entries() devuelve un array con las entries en forma [propiedad, valor] del objeto enviado como argumento.
+
+// const usuario = {
+//     name: "Andres",
+//     email: "andres@correo.com",
+//     age: 23
+// }
+
+// Object.entries(usuario) 
+/* 
+[
+  [ 'name', 'Andres' ],
+  [ 'email', 'andres@correo.com' ],
+  [ 'age', 23 ]
+]  
+*/
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,28 @@
+const countries = {MX: 'México', CD: 'Canada', CL: 'Columbia', BL: 'Bolivia'};
+
+console.log(Object.keys(countries));
+console.log(Object.values(countries));
+
+// Obtener las propiedades de un objeto en un array
+// Object.keys() devuelve un array con las propiedades (keys) del objeto enviado como argumento.
+
+// const usuario = {
+//     name: "Andres",
+//     email: "andres@correo.com",
+//     age: 23
+// }
+
+// Object.keys(usuario) 
+// // [ 'name', 'email', 'age' ]
+// Obtener los valores de un objeto en un array
+// Object.values() devuelve un array con los valores de cada propiedad del objeto enviado como argumento.
+
+// const usuario = {
+//     name: 'Andres',
+//     email: "andres@correo.com",
+//     age: 23
+// }
+
+// Object.values(usuario) 
+// // [ 'Andres', 'andres@correo.com', 23 ]
+

02a839dbdf67b51bada1f86a62386cbe2d294917
will_08 <wbahoque71@gmail.com>
2022-11-03 17:22:37 -0500

    APrendi sobre globalThis y matchAll

---
+++ src/es11/05-matchall.js
@@ -0,0 +1,39 @@
+const regex = /\b(Apple)+\b/g;
+
+const fruit  = 'Apple, Banana, Kiwi, Apple, orange, etc. etc etc.';
+
+for (const match of fruit.matchAll(regex)){
+    console.log(match);
+}
+
+// El método matchAll de los strings devuelve un iterable con todas las coincidencias del string específico a partir de una expresión regular, colocada como argumento.
+
+// string.matchAll(regex)
+
+// En el iterable, existe una propiedad denominada index con el índice del string donde la búsqueda coincide.
+
+// const regex = /\b(Apple)+\b/g
+
+// const fruit = "Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc."
+
+// Tranformación del iterable retornado a array
+// const array = [...fruit.matchAll(regex)]
+// console.log(array)
+/*
+[
+  [
+    'Apple',
+    'Apple',
+    index: 0,
+    input: 'Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc.',
+    groups: undefined
+  ],
+  [
+    'Apple',
+    'Apple',
+    index: 21,
+    input: 'Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc.',
+    groups: undefined
+  ]
+]
+*/
